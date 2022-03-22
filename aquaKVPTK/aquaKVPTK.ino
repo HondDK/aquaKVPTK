418730217c4befcf2d3caed09ded4a4a99b661d1
HondDK <78250871+HondDK@users.noreply.github.com>
2022-03-22 11:45:09 +0600

    Add files via upload

--- aquaKVPTK/aquaKVPTK.ino
+++ aquaKVPTK/aquaKVPTK.ino
@@ -2,6 +2,11 @@
 //АКВАРИУМ ПРОЕКТ 
 #include <Wire.h>
 
+#include <ESP8266WebServer.h>                             // подключение сервера 
+#include <ESP8266WiFi.h>                                  // для потключения интернета 
+#include <FS.h>                                           // для работы с файловой системой 
+#include <ESP8266FtpServer.h>                             // для работы с сервером 
+
 #include <iarduino_RTC.h>   
 iarduino_RTC RTC(RTC_DS3231);  
 uint8_t D, M, Y, h, m, s, W;    
@@ -14,15 +19,8 @@ const char* sysD=__DATE__;                                // Получаем д
 //  Парсим полученные значения sysT и sysD в массив i:    // Определяем массив «i» из 6 элементов типа int, содержащий следующие значения: секунды, минуты, часы, день, месяц и год компиляции скетча.
 const int i[6] {(sysT[6]-48)*10+(sysT[7]-48), (sysT[3]-48)*10+(sysT[4]-48), (sysT[0]-48)*10+(sysT[1]-48), (sysD[4]-48)*10+(sysD[5]-48), ((int)memmem(strM,36,sysD,3)+3-(int)&strM[0])/3, (sysD[9]-48)*10+(sysD[10]-48)};
 
-#include <ESP8266WebServer.h> // подключение сервера 
-#include <ESP8266WiFi.h> // для потключения интернета 
-#include <FS.h> // для работы с файловой системой 
-#include <ESP8266FtpServer.h> // для работы с сервером 
 
 
-
-
-  
 
 const byte relayPin1 = D1;     // Пин к которому подключен реле Свет аквариума
 const byte relayPin2 = D2;     // Свет ультрафи 
@@ -57,23 +55,30 @@ void setup()
     RTC.settime(i[0],i[1],i[2],i[3],i[4],i[5]);         // Устанавливаем время в модуль: i[0] сек, i[1] мин, i[2] час, i[3] день, i[4] месяц, i[5] год, без указания дня недели.
 //  RTC.settime(i[0],i[1],i[2],i[3],i[4],i[5], 2);      // Можно установить время с указанием дня недели, где последний параметр, это день недели (указывается вручную) в формате: 0-воскресенье, 1-понедельник, ... , 6-суббота
 
-
-
-
+  pinMode(relayPin1, OUTPUT);               // Указываем вывод RELAY как выход //cвет в аквариуме
+  pinMode(relayPin2, OUTPUT);               // Свет ультрафи 
+  pinMode(relayPin3, OUTPUT);               // Воздух к растениям
+  pinMode(relayPin4, OUTPUT);               // Вода из аквариума к растениям 
+  pinMode(relayPin5, OUTPUT);               // Вода от растений в аквариум
+  pinMode(relayPin6, OUTPUT);               // Кормушка рыб
+     
+  digitalWrite(relayPin1, LOW); 
+  digitalWrite(relayPin2, LOW);  
+  digitalWrite(relayPin3, LOW);  
+  digitalWrite(relayPin4, LOW);  
+  digitalWrite(relayPin5, LOW);  
+  digitalWrite(relayPin6, LOW);   
   
-  pinMode(relayPin1, OUTPUT);// Указываем вывод RELAY как выход //cвет в аквариуме
-  digitalWrite(relayPin1, LOW);  
-  
-  Serial.begin(9600); // вывод данных на сервер 
+  Serial.begin(9600);                      // вывод данных на сервер 
 
-  WiFi.softAP(ssid); // Создаем точку доступа 
+  WiFi.softAP(ssid);                       // Создаем точку доступа 
 
-  SPIFFS.begin();// инициализация работы с файловой системой 
-  HTTP.begin();// инициализация веб сервера 
-  ftpSrv.begin("relayPin1","relayPin1");// подняние фтп сервера 
+  SPIFFS.begin();                          // инициализация работы с файловой системой 
+  HTTP.begin();                            // инициализация веб сервера 
+  ftpSrv.begin("relayPin1","relayPin1");   // подняние фтп сервера 
 
   Serial.print("\nMy IP connect via Web-Browser or FTP: ");
-  Serial.println(WiFi.softAPIP()); // вывод локального айпи 
+  Serial.println(WiFi.softAPIP());         // вывод локального айпи 
   Serial.println("\n");
 
 
@@ -90,35 +95,6 @@ void setup()
     HTTP.send(404, "text/plain", "Not Found"); 
   });
 
-
-
-
-
-  
-
-   
- /* pinMode(relay2,OUTPUT);// Указываем вывод RELAY как выход //cвет ультрафиолет
-  digitalWrite(relay2, LOW);   
-  
-  pinMode(relay3,OUTPUT);// Указываем вывод RELAY как выход //воздух растениям
-  digitalWrite(relay3, LOW);   
-
-  pinMode(relay4,OUTPUT);// Указываем вывод RELAY как выход //вода из аквариума к растениям
-  digitalWrite(relay4, LOW);   
-  
-  pinMode(relay5,OUTPUT);// Указываем вывод RELAY как выход //вода от растений в аквариум
-  digitalWrite(relay5, LOW);   
-
-  pinMode(relay6,OUTPUT);// Указываем вывод RELAY как выход //температура воды
-  digitalWrite(relay6, LOW);   
- 
-  pinMode(relay7,OUTPUT);// Указываем вывод RELAY как выход //кормление рыб
-  digitalWrite(relay7, LOW);   
-*/
-
-
-
-
 }
 
 void loop()
@@ -130,30 +106,30 @@ void loop()
  
   
  
-  if(millis()%1000==0){                                 // Если прошла 1 секунда
-        Serial.println(RTC.gettime("d-m-Y, H:i:s, D")); // Выводим время в монитор, одной строкой
+  if(millis()%1000==0){                                   // Если прошла 1 секунда
+        Serial.println(RTC.gettime("d-m-Y, H:i:s, D"));   // Выводим время в монитор, одной строкой
         delay(2);                                         // Приостанавливаем скетч на 2 мс.
     }
     
-   if(millis()%1000==0){                                 // если прошла 1 секунда
-        RTC.gettime();                                  // Считываем текущее время из модуля.
-        D = RTC.day;                                    // Получаем текущий день месяца 1-31.
-        M = RTC.month;                                  // Получаем текущий месяц       1-12.
-        Y = RTC.year;                                   // Получаем текущий год         0-99.
-        h = RTC.Hours;                                  // Получаем текущие часы        0-23.
-        m = RTC.minutes;                                // Получаем текущие минуты      0-59.
-        s = RTC.seconds;                                // Получаем текущие секунды     0-59.
-        W = RTC.weekday;                                // Получаем текущий день недели 0-6.
+   if(millis()%1000==0){                                  // если прошла 1 секунда
+        RTC.gettime();                                    // Считываем текущее время из модуля.
+        D = RTC.day;                                      // Получаем текущий день месяца 1-31.
+        M = RTC.month;                                    // Получаем текущий месяц       1-12.
+        Y = RTC.year;                                     // Получаем текущий год         0-99.
+        h = RTC.Hours;                                    // Получаем текущие часы        0-23.
+        m = RTC.minutes;                                  // Получаем текущие минуты      0-59.
+        s = RTC.seconds;                                  // Получаем текущие секунды     0-59.
+        W = RTC.weekday;                                  // Получаем текущий день недели 0-6.
         Serial.println((String) D+"-"+M+"-"+Y+", "+h+":"+m+":"+s+", "+W); // Выводим время в монитор, одной строкой.
         delay(2);                                         // приостанавливаем скетч на 2 мс.
     }
    
-  //RTC.setAlarm(ALM1_MATCH_HOURS, 0, 0, 8, 0); //свет с 8 часов + ульрафи
+  //RTC.setAlarm(ALM1_MATCH_HOURS, 0, 0, 8, 0);           //свет с 8 часов + ульрафи
   if(h == 8 && m == 0){
    digitalWrite(relayPin1, HIGH);   
    relaySTATE1 = HIGH;     
   }
- // RTC.setAlarm(ALM2_MATCH_HOURS, 0, 0, 20, 0); //выключение света с 8 часов вечера + ульрафи
+ // RTC.setAlarm(ALM2_MATCH_HOURS, 0, 0, 20, 0);          //выключение света с 8 часов вечера + ульрафи
   if(h == 20 && m == 0){
     digitalWrite(relayPin1, LOW);
     relaySTATE1 = LOW;   
@@ -220,10 +196,6 @@ void loop()
     }
 
 
- 
-  //digitalWrite(RTCPowerPin, LOW); //Выключение питания RTC
-  //digitalWrite(RTCPowerPin, HIGH);
-  
 }
 
 //переключение реле 
