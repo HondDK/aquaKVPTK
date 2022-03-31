3db196515aa4f3b636076884f56ed20f3ca03c71
HondDK <78250871+HondDK@users.noreply.github.com>
2022-03-31 09:26:32 +0600

    Update script.js

--- aquaKVPTK/data/script.js
+++ aquaKVPTK/data/script.js
@@ -347,53 +347,7 @@ document.addEventListener("DOMContentLoaded", relay_state_temp);
 document.addEventListener("DOMContentLoaded", auqa_temp);
 document.addEventListener("DOMContentLoaded", status_time);
 document.addEventListener("DOMContentLoaded", timer_time);
-document.addEventListener('DOMContentLoaded', () => {
- 
-	// Кнопка по которой происходит клик
-	let callBackButton = document.getElementById('callback-button');
-   
-	// Модальное окно, которое необходимо открыть
-	let modal1 = document.getElementById('modal-1');
-   
-	// Кнопка "закрыть" внутри модального окна
-	let closeButton = modal1.getElementsByClassName('modal__close-button')[0];
-   
-	// Тег body для запрета прокрутки
-	let tagBody = document.getElementsByTagName('body');
-   
-	callBackButton.onclick = function (e) {
-	  e.preventDefault();
-	  modal1.classList.add('modal_active');
-	  tagBody.classList.add('hidden');
-	}
-   
-	closeButton.onclick = function (e) {
-	  e.preventDefault();
-	  modal1.classList.remove('modal_active');
-	  tagBody.classList.remove('hidden');
-	}
-   
-	modal1.onmousedown = function (e) {
-	  let target = e.target;
-	  let modalContent = modal1.getElementsByClassName('modal__content')[0];
-	  if (e.target.closest('.' + modalContent.className) === null) {
-		this.classList.remove('modal_active');
-		tagBody.classList.remove('hidden');
-	  }
-	};
-   
-	// Вызов модального окна несколькими кнопками на странице
-	let buttonOpenModal1 = document.getElementsByClassName('get-modal_1');
-   
-	for (let button of buttonOpenModal1) {
-	  button.onclick = function (e) {
-		e.preventDefault();
-		modal1.classList.add('modal_active');
-		tagBody.classList.add('hidden');
-	  }
-	}
-   
-  });
+
 
 relay_lighting.addEventListener("click", relay_inverse_lighting);
 relay_lighting_purple.addEventListener("click", relay_inverse_lighting_purple);
